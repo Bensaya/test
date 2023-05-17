bfaad802925a9ebd093d61f4769b176ae31cb5bb
Bensaya <dbenesya@gmail.com>
2023-05-17 18:41:43 +0300

    calculate var

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def calculate_median(numbers):
         return (sorted_numbers[n // 2 - 1] + sorted_numbers[n // 2]) / 2
     else:
         return sorted_numbers[n // 2]
+def calculate_variance(numbers):
+    average = calculate_average(numbers)
+    squared_differences = [(x - average) ** 2 for x in numbers]
+    return calculate_average(squared_differences)
 data = [1, 2, 3, 4, 5]
 median = calculate_median(data)
 print(f"Median: {median}")
